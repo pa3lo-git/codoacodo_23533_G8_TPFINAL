99b22d23c3b679876dc72e079fd1434848eddb70
pa3lo-git <142315638+pa3lo-git@users.noreply.github.com>
2023-12-09 23:40:54 -0300

    cambio url principal

--- Prueba/Prueba/urls.py
+++ Prueba/Prueba/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('recetas/', include('web.urls')),
+    path('', include('web.urls')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
